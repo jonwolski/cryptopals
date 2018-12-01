e067815a7e0aa7e706cb928fc8aaaf6c2f024bb7
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2018-12-01 15:32:14 -0800

    exercise 1: hex_to_base64

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+extern crate hex;
+extern crate base64;
+
+fn hex_to_base64(input: &str) -> Result<String, hex::FromHexError> {
+    use base64::encode;
+    use hex::decode;
+    Ok(encode(& decode(input)?))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hex_to_base64_test() {
+        assert_eq!(
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+            hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap(),
+            );
+    }
+}

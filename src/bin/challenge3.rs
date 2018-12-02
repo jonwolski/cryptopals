29b5c9191c5cd0ca7a36b57bca57e886d85da224
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2018-12-02 08:41:36 -0800

    Refactor to `map` ... `fold`
    Refactor from `for` loop + post-processing to `map` + `fold`.
    
    Introduce `ComputedXor` struct to keep track of results (instead of
    tracking three values in parallel).

--- src/bin/challenge3.rs
+++ src/bin/challenge3.rs
@@ -2,35 +2,62 @@ extern crate cryptopals;
 extern crate hex;
 
 use cryptopals::*;
+use std::fmt;
+
+struct ComputedXor {
+    mask: u8,
+    score: f32,
+    value: Vec<u8>,
+}
+
+impl ComputedXor {
+    fn new() -> ComputedXor {
+        ComputedXor {
+            mask: 0,
+            score: 0.0,
+            value: Vec::new(),
+        }
+    }
+}
+
+impl fmt::Display for ComputedXor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "mask: {}, score: {:000.3}, val: {}",
+            self.mask,
+            self.score,
+            String::from_utf8_lossy(&self.value)
+        )
+    }
+}
 
 pub fn main() {
     let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     let input_bytes = hex::decode(input).unwrap();
 
-    let mut high_score = 0.0;
-    let mut out_string = Vec::new();
-    let mut best_mask = 0;
+    let best_mask = (0..=255)
+        .map(|mask| {
+            let mut score = 0.0;
+            let value = input_bytes
+                .iter()
+                .map(|b| {
+                    let unmasked = b ^ mask;
+                    score += LETTER_FREQUENCIES.get(&(unmasked as char)).unwrap_or(&0.0);
+                    unmasked
+                }).collect();
+            ComputedXor {
+                score: score,
+                value: value,
+                mask: mask,
+            }
+        }).fold(ComputedXor::new(), |acc, x| {
+            if x.score > acc.score {
+                x
+            } else {
+                acc
+            }
+        });
 
-    for mask in 0..=255 {
-        let mut score = 0.0;
-        let out: Vec<u8> = input_bytes
-            .iter()
-            .map(|b| {
-                let unmasked = b ^ mask;
-                score += LETTER_FREQUENCIES.get(&(unmasked as char)).unwrap_or(&0.0);
-                unmasked
-            }).collect();
-
-        if score > high_score {
-            high_score = score;
-            out_string = out;
-            best_mask = mask;
-        }
-    }
-
-    println!(
-        "mask: {}, val: {}",
-        best_mask,
-        String::from_utf8_lossy(&out_string)
-    );
+    println!("{}", best_mask);
 }

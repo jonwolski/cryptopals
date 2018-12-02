c86b033d4c83271e24924791946df5b133b93725
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2018-12-01 18:15:20 -0800

    exercise 2: fixed_xor

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 extern crate base64;
 extern crate hex;
 
+#[allow(dead_code)]
 fn hex_to_base64(input: &str) -> Result<String, hex::FromHexError> {
     use base64::encode;
     use hex::decode;
     Ok(encode(&decode(input)?))
+}
+
+#[allow(dead_code)]
+fn fixed_xor(input: &[u8], mask: &[u8]) -> Vec<u8> {
+    input.iter().zip(mask).map(|(i, m)| i ^ m).collect()
 }
 
 #[cfg(test)]
@@ -17,5 +23,16 @@ mod tests {
             "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
             hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap()
             );
+    }
+
+    #[test]
+    fn fixed_xor_test() {
+        assert_eq!(
+            hex::decode("746865206b696420646f6e277420706c6179").unwrap(),
+            fixed_xor(
+                &hex::decode("1c0111001f010100061a024b53535009181c").unwrap(),
+                &hex::decode("686974207468652062756c6c277320657965").unwrap()
+            )
+        );
     }
 }

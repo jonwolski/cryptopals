4e4f78b3c8c46a057bb5915a75e5e0ce339143b5
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2018-12-02 10:30:20 -0800

    Extracted most of challenge3 to `xor` module

--- src/bin/challenge3.rs
+++ src/bin/challenge3.rs
@@ -1,69 +1,7 @@
-extern crate cryptopals;
 extern crate hex;
-extern crate rayon;
+extern crate cryptopals;
 
-use cryptopals::*;
-use rayon::prelude::*;
-use std::fmt;
-
-pub struct ComputedXor {
-    mask: u8,
-    score: f32,
-    value: Vec<u8>,
-}
-
-impl ComputedXor {
-    fn new() -> ComputedXor {
-        ComputedXor {
-            mask: 0,
-            score: 0.0,
-            value: Vec::new(),
-        }
-    }
-}
-
-impl fmt::Display for ComputedXor {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "score: {:000.3}, mask: {}, val: {}",
-            self.score,
-            self.mask,
-            String::from_utf8_lossy(&self.value)
-            )
-    }
-}
-
-pub fn brute_force_xor(input: &[u8]) -> ComputedXor {
-    // rayon par_iter is not implemented for InclusiveRange
-    (0..0x100)
-        .into_par_iter()
-        .map(|mask_int| {
-            let mask = mask_int as u8;
-            let mut score = 0.0;
-            let value = input
-                .iter()
-                .map(|b| {
-                    let unmasked = b ^ mask;
-                    score += LETTER_FREQUENCIES.get(&(unmasked as char)).unwrap_or(&0.0);
-                    unmasked
-                }).collect();
-            ComputedXor {
-                score: score,
-                value: value,
-                mask: mask,
-            }
-        }).reduce(
-            ComputedXor::new,
-            |acc, x| {
-                if x.score >= acc.score {
-                    x
-                } else {
-                    acc
-                }
-            },
-            )
-}
+use cryptopals::xor::*;
 
 pub fn main() {
     let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
@@ -72,20 +10,4 @@ pub fn main() {
     let output = brute_force_xor(&input_bytes);
 
     println!("{}", output);
-}
-
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    // Exercise 3
-    #[test]
-    fn brute_force_xor_test() {
-        let input = hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap();
-        let output = brute_force_xor(&input);
-        assert_eq!( 88, output.mask);
-        assert_eq!( "Cooking MC's like a pound of bacon", String::from_utf8_lossy(&output.value));
-    }
 }

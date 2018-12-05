eca4b2e2da6ef2ae75458618bc4cceff0b230b31
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2018-12-05 07:36:19 -0800

    XOR with repeated key

--- src/xor.rs
+++ src/xor.rs
@@ -69,6 +69,14 @@ pub fn brute_force_xor(input: &[u8]) -> Vec<ComputedXor> {
     results.into_iter().rev().collect()
 }
 
+pub fn xor_repeated_key(key: &[u8], message: &[u8]) -> Vec<u8> {
+    key.into_iter()
+        .cycle()
+        .zip(message.into_iter())
+        .map(|(k, v)| v ^ k)
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -87,5 +95,11 @@ mod tests {
             "Cooking MC's like a pound of bacon",
             String::from_utf8_lossy(&output.value)
         );
+    }
+
+    // Exercise 5
+    #[test]
+    fn xor_repeated_key_test() {
+        assert_eq!("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f", hex::encode(&xor_repeated_key(b"ICE", b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")));
     }
 }

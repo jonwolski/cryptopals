5ad5d150342b8efb26ec07dc59de16fce6ff0821
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2019-06-18 07:41:21 -0700

    squash! wip

--- src/xor.rs
+++ src/xor.rs
@@ -75,25 +75,25 @@ fn guess_key_size(input: &[u8]) -> Option<usize> {
 
 
 pub fn brute_force_xor_repeating<'a>(input: &[u8]) -> Vec<ComputedXor> {
-    let _best_key_size = guess_key_size(input);
-//    if let Some((_, keysize)) = best_keysize {
-//
-//        let mut stripes = vec![Vec::with_capacity(input.len()/keysize + 1); keysize];
-//        input.iter().enumerate().map(|(i, c)| {
-//            stripes[i % keysize].push(*c)
-//        });
-//        let stripes = stripes;
-//        let decoded_stripes = stripes.iter().filter_map(|s| {
-//            let result = brute_force_xor(s).first();
-//            result
-//        }).collect::<Vec<_>>();
-//        let output = Vec::with_capacity(input.len());
-//        decoded_stripes.iter().map(|cx| cx.value ).collect::<Vec<_>>().flatten()
-//
-//    } else {
-//        Vec::new()
-//    }
-    Vec::new()
+    let best_key_size = guess_key_size(input);
+    if let Some(keysize) = best_key_size {
+        println!("keysize: {:?}", keysize);
+        let mut stripes = vec![Vec::with_capacity(input.len()/keysize + 1); keysize];
+        //println!("{:?}", stripes);
+        input.iter().enumerate().for_each(|(i, c)| {
+            stripes[i % keysize].push(*c)
+        //    ;println!("{:?}", stripes)
+        });
+        println!("{:?}", stripes);
+        stripes.iter().filter_map(|s| {
+        //    println!("{:?}", s);
+            let mut results = brute_force_xor(s);
+        //    println!("brute_force_xor({:?}): {:?}", s, results);
+            results.pop()
+        }).collect::<Vec<_>>()
+    } else {
+        Vec::new()
+    }
 }
 
 pub fn brute_force_xor(input: &[u8]) -> Vec<ComputedXor> {
@@ -153,18 +153,25 @@ mod tests {
         assert_eq!("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f", hex::encode(&xor_repeated_key(b"ICE", b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")));
     }
 
+    // Exercise 5
+    #[test]
+    fn xor_repeated_key_longer_key_test() {
+        assert_eq!("0b16172d1b0b064d7404034857000548300c1063130c0f4a27411f111e0a0848280d01631c0c0c0f3f04642d570e0c482a1104390b451605360f4e2d570106093b430463111c0c0f320d", hex::encode(&xor_repeated_key(b"IceCreamSandwich", b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")));
+    }
+
     // Exercise 6
-    //#[test]
+    #[test]
     #[allow(dead_code)]
     fn brute_force_xor_repeating_test() {
-        let input =
-            hex::decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
-            .unwrap();
+        let corpus = "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?";
+        let key = "IceCreamSandwich";
+        let cipher_text = hex::encode(&xor_repeated_key(key.as_bytes(), corpus.as_bytes()));
+        let input = hex::decode(cipher_text).unwrap();
         let results = brute_force_xor_repeating(&input);
         let maybe_output = results.first();
         let output = maybe_output.unwrap();
         assert_eq!(
-            "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+            corpus,
             String::from_utf8_lossy(&output.value)
         );
     }
@@ -172,11 +179,9 @@ mod tests {
     #[test]
     fn guess_key_size_test() {
         let input =
-//            hex::decode("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
-            hex::decode("14343c2725222676662b24606c2830613726396c203f2f693d6c3d343f2225692d2225762f27242e20245c086e2e236c22242034306c3b29332f6e006c242437336e286c2f383b232f25")
+            hex::decode("0b16172d1b0b064d7404034857000548300c1063130c0f4a27411f111e0a0848280d01631c0c0c0f3f04642d570e0c482a1104390b451605360f4e2d570106093b430463111c0c0f320d")
             .unwrap();
         let actual = guess_key_size(&input);
-        println!("actual calculated value: {:?}", actual);
-        assert_eq!(Some(7), actual);
+        assert_eq!(Some("IceCreamSandwich".len()), actual);
     }
 }
